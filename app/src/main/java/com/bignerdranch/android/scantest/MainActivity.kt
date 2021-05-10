8fcff09165057364fa3ac22e632841499e74cdbf
Marina <quriza@gmail.com>
2021-05-10 22:32:21 +0300

    started

---
+++ app/src/main/java/com/bignerdranch/android/scantest/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.bignerdranch.android.scantest
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        scanBtn.setOnClickListener {
+            val activity = this@MainActivity
+
+            val intent = Intent(activity, LiveBarcodeScanningActivity::class.java)
+            startActivityForResult(intent, 400)
+
+            // activity.startActivity(Intent(activity, LiveBarcodeScanningActivity::class.java))
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        this.barcodes.text = "Отсканированные баркоды :"+ data?.getStringExtra("barcodes") ?: "ничего не было отсканировано"
+      //  Log.e("xxx", "finished")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        if (!Utils.allPermissionsGranted(this)) {
+            Utils.requestRuntimePermissions(this)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
